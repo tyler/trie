42c9cdcd6c584bda3a5fa0440e9fb5f1a8b12d87
Tyler McMullen <tyler@scribd.com>
2009-03-09 12:19:49 -0700

    Remove libdatrie from the gem.  If people want a larger word limit, they'll have to hack the source and install it manually.  Trying to make that work was... painful.

--- ext/trie/extconf.rb
+++ ext/trie/extconf.rb
@@ -1,10 +1,7 @@
-path_to_libdatrie = "#{File.dirname(__FILE__)}/../libdatrie"
-system("cd #{path_to_libdatrie} && ./configure && make")
-
 require 'mkmf'
 dir_config 'trie'
 
-unless find_library('datrie',nil, path_to_libdatrie)
+unless find_library('datrie',nil, '/usr/local/lib', '/usr/lib')
   puts 'Need libdatrie.'
   exit
 end
