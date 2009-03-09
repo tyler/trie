377b9d493b45301b70b23f61c29815f9bb993848
Tyler McMullen <tyler@scribd.com>
2009-03-08 22:19:55 -0700

    Ditto for the library path

--- ext/trie/extconf.rb
+++ ext/trie/extconf.rb
@@ -1,9 +1,10 @@
-system("cd #{File.dirname(__FILE__)}/../libdatrie && ./configure && make")
+path_to_libdatrie = "#{File.dirname(__FILE__)}/../libdatrie"
+system("cd #{path_to_libdatrie} && ./configure && make")
 
 require 'mkmf'
 dir_config 'trie'
 
-unless find_library('datrie',nil,'ext/libdatrie')
+unless find_library('datrie',nil, path_to_libdatrie)
   puts 'Need libdatrie.'
   exit
 end
